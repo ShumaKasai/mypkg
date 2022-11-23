b10e0beac9367417d4e422999670ac54d412bdfa
Shuma Kasai <kasai.0124.sss@gmail.com>
2022-11-23 22:49:49 +0900

    Initial Commit

---
+++ mypkg/talker.py
@@ -0,0 +1,16 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.msg import Query
+
+def cb(request, response):
+    if request.name == "葛西柊摩":
+        request.age = 19
+    else:
+        response.age = 255
+
+    return response
+
+rclpy.init()
+node = Node("talker")
+srv = node.create_service(Query, "query", cb)
+rclpy.spin(node)
